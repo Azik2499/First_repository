c4883186d5e7ad1e2ef2a3aefdebee41e3a8b3a0
Azik2499 <galikov.id.182@mail.ru>
2022-04-09 13:27:16 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// let a = prompt('Вводите число a!', '');
+// let b = prompt('Вводите число b!', '');
+
+
+let login = prompt('Ваш логин', '')
+
+(login == 'Сотрудник') ? message = 'Привет': (login == 'Директор') ? message = 'Здравствуйте' : (login == '') ? message = 'Нет логина' : message = '';
+
+alert (massage)  
