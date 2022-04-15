21016906d24064dad0eea286fde507eece113988
Azik2499 <salievabdulaziz99@gmail.com>
2022-04-15 16:58:01 +0300

    add link css

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 // let b = prompt('Вводите число b!', '');
 
 
-let login = prompt('Ваш логин', '')
+// let login = prompt('Ваш логин', '')
 
-(login == 'Сотрудник') ? message = 'Привет': (login == 'Директор') ? message = 'Здравствуйте' : (login == '') ? message = 'Нет логина' : message = '';
+// (login == 'Сотрудник') ? message = 'Привет': (login == 'Директор') ? message = 'Здравствуйте' : (login == '') ? message = 'Нет логина' : message = '';
 
-alert (massage)  
+// alert (massage)  
